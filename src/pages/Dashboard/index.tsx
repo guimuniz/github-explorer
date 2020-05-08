0c863a28142ec7c6e8ff9a2ad8f12ee1fa25cda5
Guilherme Muniz de Castro <guisp1901@gmail.com>
2020-05-07 23:51:42 -0300

    create dashboard template

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/17928196?s=460&u=88381e004a0af60587ce5bfa2204c3aadaa7ae28&v=4"
+            alt="Guilherme"
+          />
+
+          <div>
+            <strong>Corona</strong>
+            <p>corona rep</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/17928196?s=460&u=88381e004a0af60587ce5bfa2204c3aadaa7ae28&v=4"
+            alt="Guilherme"
+          />
+
+          <div>
+            <strong>Corona</strong>
+            <p>corona rep</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/17928196?s=460&u=88381e004a0af60587ce5bfa2204c3aadaa7ae28&v=4"
+            alt="Guilherme"
+          />
+
+          <div>
+            <strong>Corona</strong>
+            <p>corona rep</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
